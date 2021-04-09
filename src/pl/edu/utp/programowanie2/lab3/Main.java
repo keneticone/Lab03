135c5b445c905d7fd2f71f1fab1dd817486cdc8f
keneticone <maciej83agmail.com>
2021-04-09 17:57:37 +0200

    lab03

---
+++ src/pl/edu/utp/programowanie2/lab3/Main.java
@@ -0,0 +1,41 @@
+package pl.edu.utp.programowanie2.lab3;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void  main (String[] args) {
+
+        // #2
+        Task task1 = new Task("name1", "desc1", Task.TaskCategory.NEW, LocalDateTime.of(2021, 4,9, 12,0,1));
+        Task task2 = new Task("name2", "desc2", Task.TaskCategory.IN_PROGRESS, LocalDateTime.of(2021, 4,9, 13,0,0));
+        Task task3 = new Task("name3", "desc3", Task.TaskCategory.DONE, LocalDateTime.of(2021, 4,9, 14,0,0));
+        System.out.println(task1);
+        System.out.println(task2);
+        System.out.println(task3);
+
+        // #3
+        Task task4 = new Task("name4", "desc4", Task.TaskCategory.DONE, LocalDateTime.of(2021, 4,9, 15,0,0));
+        System.out.println(task4);
+        task4.setTaskDateTimeStop(task4.getTaskDateTimeStop().plusMonths(3));
+        System.out.println(task4.toString());
+
+        // #4
+//        ArrayList<Task> tasks = new ArrayList<>();
+//        tasks.add(task1);
+//        tasks.add(task2);
+//        tasks.add(task3);
+//        tasks.add(task4);
+        List<Task> tasks = Arrays.asList(task1, task2, task3, task4);
+        System.out.println("Taski z fora");
+        for (Task t:tasks) {
+            System.out.println(t);
+        }
+
+
+    }
+
+}
---
+++ src/pl/edu/utp/programowanie2/lab3/Task.java
@@ -0,0 +1,77 @@
+package pl.edu.utp.programowanie2.lab3;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+    enum TaskCategory {
+        NEW,
+        IN_PROGRESS,
+        DONE
+    }
+
+    private String taskName;
+    private String taskDescription;
+    private TaskCategory taskCategory;
+    private LocalDateTime taskDateTimeStart;
+    private LocalDateTime taskDateTimeStop;
+
+    public Task(String taskName, String taskDescription, TaskCategory taskCategory, LocalDateTime taskDateTimeStop) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskCategory = taskCategory;
+        this.taskDateTimeStart = LocalDateTime.now();
+        this.taskDateTimeStop = taskDateTimeStop;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public TaskCategory getTaskCategory() {
+        return taskCategory;
+    }
+
+    public void setTaskCategory(TaskCategory taskCategory) {
+        this.taskCategory = taskCategory;
+    }
+
+    public LocalDateTime getTaskDateTimeStart() {
+        return taskDateTimeStart;
+    }
+
+    public void setTaskDateTimeStart(LocalDateTime taskDateTimeStart) {
+        this.taskDateTimeStart = taskDateTimeStart;
+    }
+
+    public LocalDateTime getTaskDateTimeStop() {
+        return taskDateTimeStop;
+    }
+
+    public void setTaskDateTimeStop(LocalDateTime taskDateTimeStop) {
+        this.taskDateTimeStop = taskDateTimeStop;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskName='" + taskName + '\'' +
+                ", taskDescription='" + taskDescription + '\'' +
+                ", taskCategory=" + taskCategory +
+                ", taskDateTimeStart=" + taskDateTimeStart +
+                ", taskDateTimeStop=" + taskDateTimeStop +
+                '}';
+    }
+}
